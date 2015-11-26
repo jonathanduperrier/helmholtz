c354c000c2dab627d2a5593e26ee7184e8f3e081
jonathanduperrier <jonathan.duperrier@gmail.com>
2015-11-26 11:45:06 +0100

    ./

--- helmholtz/client/hermann/app/animal/controllers.js
+++ helmholtz/client/hermann/app/animal/controllers.js
@@ -1,35 +1,25 @@
 'use strict';
 /* Controllers */
-var mod_exp = angular.module( 'hermann.animal', [
+var mod_animal = angular.module( 'hermann.animal', [
     'ngResource',
     'ngRoute',
     'hermann.people',
     'ui.bootstrap', 
     'angularModalService',
     'mod_tlv',
-    'animalServices'
+    'animalServices',
+    'supplierServices',
+    'ngRoute',
     ]);
 
-mod_exp.controller('ListAnimal', [
-  '$scope', 'animal' ,'ModalService',
-  function($scope, animal, ModalService) {
-  	$scope.animal = animal.get({}, function(data){
-      /*$scope.animal.objects.forEach( function( elec ){
-        var $type = elec.type.split('/');
-        elec.type = $type[3];
-        var $manufacturer = elec.manufacturer.split('/');
-        elec.manufacturer = $manufacturer[3];
-        //get timeline
-        var $timeline = elec.timeline.split('/');
-        var $idTimeline = parseInt($timeline[3]);
-        elec.timeline = timeLine.get({id:$idTimeline}, function(data){
-          var $exp = data.experiment;
-          //get experiment
-          var $experiment = $exp.split('/');
-          var $idExperiment = $experiment[2];
-          elec.experiment = Experiment.get({id:$idExperiment});
-        });
-      });*/
+mod_animal.controller('ListAnimal', [
+  '$scope', '$rootScope', 'animals' ,'ModalService', '$route',
+  function($scope, $rootScope, animals, ModalService, $route) {
+    $scope.$route = $route;
+    $rootScope.spin = 0;
+  	$scope.animal = animals.get({}, function(data){
+      $scope.animal.objects.forEach( function( animal ){
+      });
     });
     $scope.predicate = 'identifier';
     $scope.reverse = false;
@@ -37,25 +27,129 @@ mod_exp.controller('ListAnimal', [
       $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
       $scope.predicate = predicate;
     };
+
+    $scope.showDlgAnimal = function(animal){
+      if( animal == null ){
+        // ADD
+        animal = {
+          id: null,
+          identifier: "",
+          nickname: "",
+          weight: "",
+          sex: "",
+          birth: new Date(),
+          sacrifice: new Date(),
+          supplier: null,
+        };
+        var edition = false;
+      } else {
+        // EDIT
+        var edition = true;
+      }
+      ModalService.showModal({
+        templateUrl: "animal/modal_dlg_animal.tpl.html",
+        controller: "ManageAnimalController",
+        inputs: {
+          title: "Animal information",
+          edition: edition,
+          animal: animal,
+        }
+      }).then(function(modal) {
+        modal.element.modal();
+            modal.close.then( function(result) {
+              if(result.del_evt == true){
+                  $scope.showConfirmRemoveAnimal(result.animal);
+              } else{
+                  $scope.manageAnimal( result.animal, edition );                    
+              }
+          });
+      });
+    };
+
+    $scope.manageAnimal = function(animal, edition){
+      angular.element(window).spin();
+      $rootScope.spin = 1;
+      if(edition == false){
+        animals.post(animal, function(data){
+          $scope.stopSpin();
+          $scope.$route.reload();
+        });
+      } else {
+        animals.put({id:animal.id}, angular.toJson(animal), function(){
+            $scope.stopSpin();
+            $scope.$route.reload();
+        });
+      }
+    };
+
+    $scope.stopSpin = function() {
+      if($rootScope.spin == 1){
+        setTimeout(function(){ angular.element(window).spin(); }, 3500);
+      }
+      $rootScope.spin = 0;
+    };
+
   }
 ]);
 
-mod_exp.controller('DetailAnimal', ['$scope', '$routeParams', 'animal' ,'ModalService', function($scope, $routeParams, animal, ModalService){
-    $scope.animal = animal.get( {id: $routeParams.eID}, function(data){
-        /*var $type = $scope.elec.type.split('/');
-        $scope.elec.type = $type[3];
-        var $manufacturer = $scope.elec.manufacturer.split('/');
-        $scope.elec.manufacturer = $manufacturer[3];
-        //get timeline
-        var $timeline = $scope.elec.timeline.split('/');
-        var $idTimeline = parseInt($timeline[3]);
-        $scope.elec.timeline = timeLine.get({id:$idTimeline}, function(data){
-          var $exp = data.experiment;
-          //get experiment
-          var $experiment = $exp.split('/');
-          var $idExperiment = $experiment[2];
-          $scope.elec.experiment = Experiment.get({id:$idExperiment});
-        });*/
+mod_animal.controller('ManageAnimalController', [
+    '$scope', '$element', 'title', 'close', 'edition', 'animal',
+    function($scope, $element, title, close, edition, animal) {
+      $scope.animal = animal;
+      $scope.title = title;
+
+      $scope.beforeClose = function() {
+        //console.log($scope.dateFormat);
+        if($scope.animal.identifier == ""){
+            $scope.msgAlert = "Identifier field is required";
+        }
+        else if($scope.animal.nickname == ""){
+          $scope.msgAlert = "Nickname field is required";
+        }
+        else if(!$.isNumeric($scope.animal.weight)){
+          $scope.msgAlert = "Weight field must be a number";
+        } 
+        else if($scope.animal.sex == ""){
+          $scope.msgAlert = "Sex field is required";
+        }
+        else if($scope.animal.birth  == ""){
+          $scope.msgAlert = "Birth field is required";
+        }
+        else if($scope.animal.sacrifice  == ""){
+          $scope.msgAlert = "Sacrifice field is required";
+        }
+        else {
+            $scope.close();
+        }
+      };
+
+      $scope.delete = function(){
+          $scope.del_evt = true;
+          $scope.close();
+      };
+
+      $scope.close = function() {
+          close({
+              animal: $scope.animal,
+              del_evt: $scope.del_evt,
+          }, 100); // close, but give 500ms for bootstrap to animate
+      };
+
+      //  This cancel function must use the bootstrap, 'modal' function because
+      //  the doesn't have the 'data-dismiss' attribute.
+      $scope.cancel = function() {
+          //  Manually hide the modal.
+          $element.modal('hide');
+          //  Now call close, returning control to the caller.
+          close({
+              animal: $scope.animal,
+          }, 100); // close, but give 500ms for bootstrap to animate
+      };
+
+}]);
+
+mod_animal.controller('DetailAnimal', ['$scope', '$routeParams', 'animals' ,'ModalService', function($scope, $routeParams, animals, ModalService){
+    $scope.animal = animals.get( {id: $routeParams.eID}, function(data){
     });
   }
 ]);
