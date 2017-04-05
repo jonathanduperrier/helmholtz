37795f8d6768945cb4625c64cb938d61297ca90e
Jonathan Duperrier <jonathan.duperrier@gmail.com>
2017-04-05 16:12:46 +0200

    20170405_16h12

--- helmholtz/client/hermann/app/protocol/controllers.js
+++ helmholtz/client/hermann/app/protocol/controllers.js
@@ -7,28 +7,40 @@ var mod_exp = angular.module( 'hermann.protocol', [
     'ui.bootstrap',
     'angularModalService',
     'mod_tlv',
-    'protocolServices'
+    'protocolServices',
+    'RecordingRecordingService',
+    'RecordingBlockService',
     ]);
 
 mod_exp.controller('ListProtocol', [
-  '$scope', '$rootScope', 'protocol' ,'ModalService', 'neuron','timeLine', 'Experiment',
-  function($scope, $rootScope, protocol, ModalService, neuron, timeLine, Experiment) {
+  '$scope', '$rootScope', 'protocol' ,'ModalService', 'neuron','timeLine', 'Experiment', 'RecordingRecordings', 'RecordingBlocks',
+  function($scope, $rootScope, protocol, ModalService, neuron, timeLine, Experiment, RecordingRecordings, RecordingBlocks) {
     $rootScope.page_title = "Protocol";
   	$scope.protocol = protocol.get({}, function(data){
       $scope.protocol.objects.forEach( function( prot ){
-        var $neur = prot.depend.split('/');
-        var $idNeur = $neur[3];
-        prot.neuron = neuron.get({id:$idNeur});
+        var rec_recording = prot.rec_recording.split('/');
+        var id_rec_recording = rec_recording[3];
+        RecordingRecordings.get({id: id_rec_recording}, function(data){
+          prot.depend = data.block;
+          prot.text = data.name;
+          prot.start = data.rec_datetime;
 
-        //get timeline
-        var $timeline = prot.timeline.split('/');
-        var $idTimeline = parseInt($timeline[3]);
-        prot.timeline = timeLine.get({id:$idTimeline}, function(data){
-          var $exp = data.experiment;
-          //get experiment
-          var $experiment = $exp.split('/');
-          var $idExperiment = $experiment[2];
-          prot.experiment = Experiment.get({id:$idExperiment});
+          var $neur = prot.depend.split('/');
+          var $idNeur = $neur[3];
+          prot.neuron = neuron.get({id:$idNeur});
+
+          //get timeline
+          var $timeline = prot.timeline.split('/');
+          var $idTimeline = parseInt($timeline[3]);
+          prot.timeline = timeLine.get({id:$idTimeline}, function(data){
+            var $exp = data.experiment;
+            //get experiment
+            var $experiment = $exp.split('/');
+            var $idExperiment = $experiment[2];
+            prot.experiment = Experiment.get({id:$idExperiment});
+            var id_depend = prot.depend.split("/");
+            prot.neuron = RecordingBlocks.get({id:id_depend[3]});
+          });
         });
       });
     });
@@ -41,7 +53,7 @@ mod_exp.controller('ListProtocol', [
   }
 ]);
 
-mod_exp.controller('DetailProtocol', ['$scope', '$rootScope', '$routeParams', 'timeLine', 'neuron', 'protocol', 'Experiment',
+mod_exp.controller('DetailProtocol', ['$scope', '$rootScope', '$routeParams', 'timeLine', 'neuron', 'protocol', 'Experiment', 'RecordingRecordings',
 function($scope, $rootScope, $routeParams, timeLine, neuron, protocol, Experiment){
   $rootScope.page_title = "Protocol";
   $scope.prot = protocol.get( {id: $routeParams.eID}, function(data){
